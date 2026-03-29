6849ee5a62365a84b0fec9ee9bcf2f55e078c59b
Shankar Teja <121328737+shankarteja@users.noreply.github.com>
2026-03-29 08:02:51 +0530

    uploaded file

---
+++ shankarbank/src/main/java/com/shankarBank/ShankarbankApplication.java
@@ -0,0 +1,13 @@
+package com.shankarBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShankarbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShankarbankApplication.class, args);
+	}
+
+}
---
+++ shankarbank/src/test/java/com/shankarBank/ShankarbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankarBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShankarbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
